b799b872412704c53c02ed7986c3b12406f902c0
João Pedro da Silva Nascimento <jpsn2@cin.ufpe.br>
2022-05-23 21:39:42 -0300

    Ajustes finais

---
+++ main.js
@@ -0,0 +1,21 @@
+const bigbox = document.querySelector(".bigbox");
+const botao = document.querySelector(".botao");
+const txt = document.getElementById("txt2");
+
+const colors = ['#F2F2F2', '#C7C7C7', '#292929'];
+const txts = ['Background: #F2F2F2', 'Background: #C7C7C7', 'Background: #292929'];
+
+let background_Bigbox = '#292929';
+
+botao.addEventListener('click', ()=>{
+    if(background_Bigbox === '#292929') {
+        background_Bigbox = '#F2F2F2'       
+    } else if(background_Bigbox === '#F2F2F2'){
+        background_Bigbox = '#C7C7C7'
+    } else {
+        background_Bigbox = '#292929'
+    }
+    
+    bigbox.style.backgroundColor = background_Bigbox;
+    txt.textContent = 'Background: ' + background_Bigbox;
+});
